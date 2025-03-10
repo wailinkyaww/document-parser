60030b1016adbe783eacc2a2dc1d6338e56c9c0c
Wai Lin Kyaw <wailink13@gmail.com>
2025-03-10 23:31:38 +0800

    initialize fast api with uvicorn

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/')
+def health_check():
+    return {"message": 'Document Parser - AI Agent is running fine!'}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get('/detect-table-bounding')
+def detect_table_bounding_box():
+    return {"table": []}
